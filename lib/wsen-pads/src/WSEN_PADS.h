3c29b1a29be2980a1be26cf2e2997224c5d451db
dumontALINE <aline.dmnt83@gmail.com>
2026-04-23 14:41:50 +0200

    fix(wsen-pads): Move public methods to public section.

--- lib/wsen-pads/src/WSEN_PADS.h
+++ lib/wsen-pads/src/WSEN_PADS.h
@@ -13,12 +13,37 @@ class WSEN_PADS {
 
     bool begin();
 
-   private:
+    uint8_t deviceId();
+
+    void reboot();
+
+    void powerOff();
+    void powerOn(uint8_t odr = ODR_1_HZ);
+    void softReset();
+
     struct ReadResult {
         float pressure;
         float temperature;
     };
+    float pressureHpa();
+    float pressurePa();
+    float pressureKpa();
+    float temperature();
+    ReadResult read();
 
+    bool setContinuous(uint8_t odr = ODR_1_HZ, bool low_noise = false, bool low_pass = false,
+                       bool low_pass_strong = false);
+
+    void triggerOneShot(bool lowNoise = false);
+    ReadResult readOneShot(bool lowNoise = false);
+
+    void setTempOffset(float offset_c);
+    bool calibrateTemperature(float refLow, float measuredLow, float refHigh, float measuredHigh);
+
+    void enableLowPass(bool strong = false);
+    void disableLowPass();
+
+   private:
     TwoWire* _wire;
     uint8_t _address;
     float _temp_gain;
@@ -37,37 +62,13 @@ class WSEN_PADS {
     bool checkDevice();
     void configureDefault();
 
-    uint8_t deviceId();
     uint8_t status();
     bool pressureReady();
     bool temperatureReady();
     bool dataReady();
 
-    void powerOff();
-    void powerOn(uint8_t odr = ODR_1_HZ);
-    void softReset();
-    void reboot();
-
     bool isPowerDown();
     bool ensureData();
     int32_t pressureRaw();
     int32_t temperatureRaw();
-
-    float pressureHpa();
-    float pressurePa();
-    float pressureKpa();
-    float temperature();
-    ReadResult read();
-
-    void triggerOneShot(bool lowNoise = false);
-    ReadResult readOneShot(bool lowNoise = false);
-
-    bool setContinuous(uint8_t odr = ODR_1_HZ, bool low_noise = false, bool low_pass = false,
-                       bool low_pass_strong = false);
-
-    void enableLowPass(bool strong = false);
-    void disableLowPass();
-
-    void setTempOffset(float offset_c);
-    bool calibrateTemperature(float refLow, float measuredLow, float refHigh, float measuredHigh);
 };
