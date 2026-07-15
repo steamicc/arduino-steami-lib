cdabed5fb97340ff96f1bbd65967b5f1719f8a98
Charly-sketch <glic.per@gmail.com>
2026-07-15 11:35:35 +0200

    feat(ism330dl): Add usage examples.
    Add example sketches demonstrating the main driver features.
    
    Examples include:
    - basicreader
    - orientation
    - motion
    
    These examples provide simple reference applications for reading sensor
    data and detecting device orientation and movement.

---
+++ lib/ism330dl/examples/BasicReader/BasicReader.ino
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <ISM330DL.h>
+#include <Wire.h>
+
+TwoWire internalI2C(I2C_INT_SDA, I2C_INT_SCL);
+ISM330DL imu(internalI2C);
+
+void setup() {
+    Serial.begin(115200);
+
+    internalI2C.begin();
+
+    if (!imu.begin()) {
+        Serial.println("ISM330DL not detected.");
+        while (true) {
+            delay(1000);
+        }
+    }
+
+    Serial.println("ISM330DL ready.");
+}
+
+void loop() {
+    ISM330DL::Vector3 accel;
+    ISM330DL::Vector3 gyro;
+    float temperature;
+
+    if (imu.accelerationG(accel) && imu.gyroscopeDps(gyro) && imu.temperature(temperature)) {
+        Serial.println("Acceleration (g)");
+        Serial.print("X: ");
+        Serial.println(accel.x, 3);
+        Serial.print("Y: ");
+        Serial.println(accel.y, 3);
+        Serial.print("Z: ");
+        Serial.println(accel.z, 3);
+
+        Serial.println();
+
+        Serial.println("Gyroscope (dps)");
+        Serial.print("X: ");
+        Serial.println(gyro.x, 2);
+        Serial.print("Y: ");
+        Serial.println(gyro.y, 2);
+        Serial.print("Z: ");
+        Serial.println(gyro.z, 2);
+
+        Serial.println();
+
+        Serial.print("Temperature: ");
+        Serial.print(temperature, 1);
+        Serial.println(" °C");
+
+        Serial.println();
+    }
+
+    delay(300);
+}
---
+++ lib/ism330dl/examples/Motion/Motion.ino
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#include <ISM330DL.h>
+#include <Wire.h>
+
+TwoWire internalI2C(I2C_INT_SDA, I2C_INT_SCL);
+ISM330DL imu(internalI2C);
+
+ISM330DL::MotionType lastMotion = ISM330DL::MotionType::STABLE;
+
+void setup() {
+    Serial.begin(115200);
+
+    internalI2C.begin();
+
+    if (!imu.begin()) {
+        Serial.println("ISM330DL not detected.");
+        while (true) {
+            delay(1000);
+        }
+    }
+
+    Serial.println("Move the board...");
+}
+
+void loop() {
+    ISM330DL::Motion motion;
+
+    if (imu.motion(motion) && motion.type != lastMotion) {
+        lastMotion = motion.type;
+
+        Serial.print("Motion: ");
+        Serial.print(ISM330DL::motionToString(motion.type));
+
+        if (motion.type != ISM330DL::MotionType::STABLE) {
+            Serial.print(" (");
+            Serial.print(motion.value, 1);
+            Serial.print(" dps)");
+        }
+
+        Serial.println();
+    }
+
+    delay(100);
+}
