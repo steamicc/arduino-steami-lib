2ffad414421a7b95bf4fc1f6719a214a73c44252
Charly-sketch <glic.per@gmail.com>
2026-07-23 10:57:43 +0200

    fix(vl53l1x): Fix various spelling mistake in BasicDistance.
    - remove include from //SPDX comment
    - Add Wait comment
    - Remake !sensor.begin loop

--- lib/vl53l1x/examples/BasicDistance/BasicDistance.ino
+++ lib/vl53l1x/examples/BasicDistance/BasicDistance.ino
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later#include <Arduino.h>
+// SPDX-License-Identifier: GPL-3.0-or-later
 #include <Arduino.h>
 #include <VL53L1X.h>
 #include <Wire.h>
@@ -9,14 +9,16 @@ VL53L1X sensor(internalI2C);
 void setup() {
     Serial.begin(115200);
     while (!Serial) {
+        // Wait up to 2 s for the host USB CDC to enumerate.
     }
 
     internalI2C.begin();
 
     if (!sensor.begin()) {
-        Serial.println("VL53L1X not found");
-        while (1)
-            ;
+        Serial.println("VL53L1X not found. Check wiring and I2C address.");
+        while (true) {
+            delay(1000);
+        }
     }
 
     sensor.startRanging();
